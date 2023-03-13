ca993d0024cc326f9d2866314d2b523b6ccc75ff
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2023-03-14 01:20:21 +0200

    feat: mui styling

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles.css';
+import './global.css';
+import { theme } from './theme/theme';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<App />
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<App />
+		</ThemeProvider>
 	</React.StrictMode>
 );
---
+++ src/theme/theme.ts
@@ -0,0 +1,18 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+	palette: {
+		primary: {
+			light: '#757ce8',
+			main: '#5e35b1',
+			dark: '#002884',
+			contrastText: '#fff',
+		},
+		secondary: {
+			light: '#ff7961',
+			main: '#f44336',
+			dark: '#ba000d',
+			contrastText: '#000',
+		},
+	},
+});
