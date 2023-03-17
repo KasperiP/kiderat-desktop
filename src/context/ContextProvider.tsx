1a0d7c0817b5cedaa40de17eec29ccb1a86323c5
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2023-03-17 19:03:00 +0200

    feat: global context

--- src/context/ContextProvider.tsx
+++ src/context/ContextProvider.tsx
@@ -1,5 +1,5 @@
 import { createContext, useState } from 'react';
-import { IUser } from '../interfaces/interfaces';
+import { IEvent, IUser } from '../interfaces/interfaces';
 
 export const GlobalContext = createContext<GlobalState | null>(null);
 
@@ -10,17 +10,34 @@ interface ContextProviderProps {
 interface GlobalState {
 	authorizationToken?: string;
 	user?: IUser;
-	setState: (state: GlobalState) => void;
+	event?: IEvent;
+	settings: {
+		amount: number | null;
+	};
+	setState: React.Dispatch<React.SetStateAction<GlobalState>>;
 }
 
+const initialState: GlobalState = {
+	settings: {
+		amount: null,
+	},
+	setState: () => {
+		return;
+	},
+};
+
 export const GlobalContextProvider = ({ children }: ContextProviderProps) => {
-	const [state, setState] = useState<GlobalState | null>(null);
+	const [state, setState] = useState<GlobalState>(initialState);
 	return (
 		<GlobalContext.Provider
 			value={{
 				authorizationToken: state?.authorizationToken,
 				user: state?.user,
+				settings: {
+					amount: state?.settings.amount,
+				},
 				setState,
+				event: state?.event,
 			}}
 		>
 			{children}
