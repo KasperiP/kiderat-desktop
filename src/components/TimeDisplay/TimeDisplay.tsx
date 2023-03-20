cee7c28b457a04551b85374d6713e083689566ff
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2023-03-20 17:17:15 +0200

    fix: timedisplay component causing crash when timer is reached.

--- src/components/TimeDisplay/TimeDisplay.tsx
+++ src/components/TimeDisplay/TimeDisplay.tsx
@@ -1,32 +1,42 @@
 import { useEffect, useState } from 'react';
 
 export const TimeDisplay = ({ date }: { date?: Date }) => {
-	const [time, setTime] = useState(date);
 	const [dateString, setDateString] = useState('');
 
-	if (date && date.getTime() < new Date().getTime()) {
-		return <>Alkanut</>;
-	}
+	const formatDateStr = (date: Date) => {
+		const now = new Date();
+		const diff = date.getTime() - now.getTime();
+		const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+		const hours = Math.floor(
+			(diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+		);
+		const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+		const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+		let str = '';
+		if (days > 0) str += `${days}d `;
+		if (hours > 0) str += `${hours}h `;
+		if (minutes > 0) str += `${minutes}m `;
+		if (seconds > 0) str += `${seconds}s `;
+		setDateString(str === '' ? '0s' : str);
+	};
 
 	// Countdown to sales start
 	useEffect(() => {
-		if (!time) return;
-		const interval = setInterval(() => {
-			const now = new Date();
-			const diff = time.getTime() - now.getTime();
-			const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-			const hours = Math.floor(
-				(diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-			);
-			const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-			const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+		if (!date) return;
+		if (date.getTime() <= new Date().getTime()) {
+			setDateString('Alkanut');
+			return;
+		}
 
-			let str = '';
-			if (days > 0) str += `${days}d `;
-			if (hours > 0) str += `${hours}h `;
-			if (minutes > 0) str += `${minutes}m `;
-			if (seconds > 0) str += `${seconds}s `;
-			setDateString(str);
+		formatDateStr(date);
+		const interval = setInterval(() => {
+			if (date.getTime() <= new Date().getTime()) {
+				setDateString('Alkanut');
+				clearInterval(interval);
+				return;
+			}
+			formatDateStr(date);
 		}, 1000);
 
 		return () => clearInterval(interval);
