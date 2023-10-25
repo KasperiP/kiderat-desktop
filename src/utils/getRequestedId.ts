ea16aa7743ad06e1c3fcdb41efbc321596e30403
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2023-10-25 16:29:30 +0300

    chore: update requested id method

--- src/utils/getRequestedId.ts
+++ src/utils/getRequestedId.ts
@@ -1,10 +1,10 @@
 export function getRequestId(inventoryId: string): string {
-	const hash = '2ad64e4b26c84fbabba58181de76f7b0';
+	const hash = '3a7ceab52edd4b7ba69e3e120be4793f';
 	return btoa(
 		[...inventoryId.replace(/-/g, '')]
 			.map((char, i) =>
 				String.fromCharCode(char.charCodeAt(0) ^ hash.charCodeAt(i))
 			)
 			.join('')
-	).substring(0, 10);
+	).substring(0, 8);
 }
